5368c43c549183f276e0d5702d277838f16180b4
Arthur Schwaiger <arthur@schwaiger-fam.net>
2025-10-14 14:12:16 +0200

    move code from web-image-interop to this project

---
+++ src/main/java/io/github/atur256/vuewebimageinterop/JSVueTemplate.java
@@ -0,0 +1,19 @@
+package io.github.atur256.vuewebimageinterop;
+
+import org.graalvm.webimage.api.JSString;
+
+
+public record JSVueTemplate(JSString html) {
+
+    public String get() {
+        return html.asString();
+    }
+
+    public JSString getJS() {
+        return html;
+    }
+
+    public static JSVueTemplate of(String rawHtml) {
+        return new JSVueTemplate(JSString.of(rawHtml));
+    }
+}
---
+++ src/main/java/io/github/atur256/vuewebimageinterop/VueRuntimeLoader.java
@@ -0,0 +1,12 @@
+package io.github.atur256.vuewebimageinterop;
+
+import org.graalvm.webimage.api.JS;
+
+
+// Used to include Vue runtime via Java if not loaded from HTML
+@JS.Code.Include("/vue.global.prod.js")
+public class VueRuntimeLoader {
+
+    public static void injectVueFile() {
+    }
+}
