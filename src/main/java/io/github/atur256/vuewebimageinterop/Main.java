9e146f1f96df00afe1fe4cd79523db4f135fd873
Arthur Schwaiger <arthur@schwaiger-fam.net>
2025-11-04 18:51:38 +0100

    update to work with new web image interop version

--- src/main/java/io/github/atur256/vuewebimageinterop/Main.java
+++ src/main/java/io/github/atur256/vuewebimageinterop/Main.java
@@ -1,19 +1,6 @@
 package io.github.atur256.vuewebimageinterop;
 
-import io.github.atur256.vuewebimageinterop.examples.advanced.setup.SetupExample;
-import io.github.atur256.vuewebimageinterop.examples.advanced.shoppinglist.viaoptionsapi.ShoppingListExample;
-import io.github.atur256.vuewebimageinterop.examples.advanced.svggraph.SVGGraphExample;
-import io.github.atur256.vuewebimageinterop.examples.basics.counter.CounterExample;
 import io.github.atur256.vuewebimageinterop.examples.basics.helloworld.HelloWorldExample;
-import io.github.atur256.vuewebimageinterop.examples.composition.componenthierarchy.ComponentHierarchyExample;
-import io.github.atur256.vuewebimageinterop.examples.composition.directivesandattrs.DirectivesAndAttrsExample;
-import io.github.atur256.vuewebimageinterop.examples.composition.exposechild.ExposeChildExample;
-import io.github.atur256.vuewebimageinterop.examples.composition.globalcomponentplugin.GlobalComponentPluginExample;
-import io.github.atur256.vuewebimageinterop.examples.composition.mixins.MixinComponent;
-import io.github.atur256.vuewebimageinterop.examples.composition.mixins.MixinsExample;
-import io.github.atur256.vuewebimageinterop.examples.reactivity.lifecycle.LifecycleHooksExample;
-import io.github.atur256.vuewebimageinterop.examples.reactivity.rendernexttick.RenderNextTickExample;
-import io.github.atur256.vuewebimageinterop.examples.reactivity.watcheffects.ReactiveWatchExample;
 
 
 /**
@@ -27,8 +14,6 @@ public class Main {
     public static void main(String[] args) {
 
         // Run a specific example
-//        HelloWorldExample.main(args);
-
-        SVGGraphExample.main(args); // TODO:
+        HelloWorldExample.main(args);
     }
 }
